32950677a196bc86b67bb7bcab0da156713497ae
Yi Xie <imxieyi@users.noreply.github.com>
2021-03-07 15:36:19 +0100

    Initial Commit

---
+++ ios-imageview-heif-alpha-bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-imageview-heif-alpha-bug
+//
+//  Created by Yi Xie on 2021/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-imageview-heif-alpha-bug/ios_imageview_heif_alpha_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_imageview_heif_alpha_bugApp.swift
+//  ios-imageview-heif-alpha-bug
+//
+//  Created by Yi Xie on 2021/3/7.
+//
+
+import SwiftUI
+
+@main
+struct ios_imageview_heif_alpha_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
